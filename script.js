775899c14ab528453f8b72a581f49fa44c4ba0d3
Marco6409 <maframontane@gmail.com>
2026-06-18 14:43:40 +0200

    Mise à jour du programme

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-/* Style vide */
+function openTab(tabName) {
+    let contents = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < contents.length; i++) {
+        contents[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
