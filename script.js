2c906d25329ba88977c2b2610c0c4c77ad432c8f
Marco6409 <maframontane@gmail.com>
2026-06-18 14:51:13 +0200

    Ajout structure complète programme premium

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function openTab(tabName) {
-    let contents = document.getElementsByClassName("tab-content");
-    for (let i = 0; i < contents.length; i++) {
-        contents[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-}
+console.log("Programme Premium chargé. Bonne séance Marc !");
