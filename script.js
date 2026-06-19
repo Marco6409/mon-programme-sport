af6d38f83ab4410f21f6ea877118134ea75eb3a1
Marco6409 <maframontane@gmail.com>
2026-06-19 16:15:00 +0200

    Mise à jour finale : ajout suivi poids et zones performance

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-console.log("Programme Premium chargé. Bonne séance Marc !");
+console.log("Programme Premium Salle 100 chargé. Bonne séance Marc !");
+
 // On sélectionne toutes les zones de saisie des charges
 const inputs = document.querySelectorAll('.input-charge');
 
-// Au chargement de la page, on récupère les valeurs sauvegardées
-inputs.forEach(input => {
-    const id = input.parentElement.parentElement.cells[0].innerText; // Utilise le nom de l'exercice comme identifiant
-    const savedValue = localStorage.getItem(id);
-    if (savedValue) {
+inputs.forEach((input, index) => {
+    // On crée une clé unique basée sur l'index de l'input dans toute la page
+    // Cela garantit qu'aucune case n'écrase une autre
+    const storageKey = 'input_' + index;
+
+    // 1. Au chargement : on récupère la valeur sauvegardée
+    const savedValue = localStorage.getItem(storageKey);
+    if (savedValue !== null) {
         input.value = savedValue;
     }
 
-    // Dès qu'on change une valeur, on la sauvegarde
-    input.addEventListener('change', () => {
-        localStorage.setItem(id, input.value);
+    // 2. Dès qu'on modifie une valeur, on la sauvegarde
+    input.addEventListener('input', () => {
+        localStorage.setItem(storageKey, input.value);
     });
 });
